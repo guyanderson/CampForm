6344602c8805919684cd9a2fb6733ed4e734c0ac
guy <luckythelich@hotmail.com>
2017-11-09 11:53:46 -0800

    change from .net.core to .net.framework. Add jquery validator. First test of site name required is working.

---
+++ CampingForm/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CampingForm.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ CampingForm/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CampingForm.MvcApplication" Language="C#" %>
